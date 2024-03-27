d4ca0f629e358859ae4f4088ea6fb7ec4464cb6c
Yakubchyk <ivan.yakubchyk@gmail.com>
2024-03-27 17:52:53 +0100

    vozvedenie v quadrat

---
+++ src/Ubung1.java
@@ -0,0 +1,18 @@
+import java.util.Random;
+
+public class Ubung1 {
+    public static void main(String[] args) {
+        double num1 = Math.random();
+        double num2 = Math.random();
+        double num3 = Math.random();
+        double num4 = Math.random();
+        double num5 = Math.random();
+
+        System.out.println(Math.pow(num1, 2));
+        System.out.println(Math.pow(num2, 2));
+        System.out.println(Math.pow(num3, 2));
+        System.out.println(Math.pow(num4, 2));
+        System.out.println(Math.pow(num5, 2));
+        
+    }
+}
