c4a77943020bbaabade7190f56e59638fc0d07ab
Yakubchyk <ivan.yakubchyk@gmail.com>
2024-03-27 17:16:48 +0100

    Arbeit mit Math

--- src/ArrClass.java
+++ src/ArrClass.java
@@ -1,5 +1,13 @@
 public class ArrClass {
     public static void main(String[] args) {
+        // Math
+        // System.out.println(Math.max(5, 6, 9, 1));
+        // System.out.println(Math.min(5, 6));
+        // System.out.println(Math.abs(-5));
+        System.out.println(Math.random());
+        System.out.println(Math.round(1.56));
+        System.out.println(Math.ceil(1.00012));
+        System.out.println(Math.floor(1.56));
 
     }
 }
