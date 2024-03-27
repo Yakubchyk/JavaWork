0093bef8ad3c98d6cbb8a4f87dccea5db0d5cf48
Yakubchyk <ivan.yakubchyk@gmail.com>
2024-03-27 17:27:04 +0100

    mit Random

--- src/ArrClass.java
+++ src/ArrClass.java
@@ -1,3 +1,5 @@
+import java.util.Random;
+
 public class ArrClass {
     public static void main(String[] args) {
         // Math
@@ -5,9 +7,16 @@ public class ArrClass {
         // System.out.println(Math.min(5, 6));
         // System.out.println(Math.abs(-5));
         System.out.println(Math.random());
-        System.out.println(Math.round(1.56));
-        System.out.println(Math.ceil(1.00012));
-        System.out.println(Math.floor(1.56));
+        // System.out.println(Math.round(1.56));
+        // System.out.println(Math.ceil(1.00012));
+        // System.out.println(Math.floor(1.56));
+        // System.out.println(Math.sqrt(9));
+        // System.out.println(Math.addExact()); TODO:check
+        // System.out.println(Math.pow(152, 21)); // возведение в степерь
+        Random random = new Random();
+        System.out.println(random.nextBoolean());
+
+
 
     }
 }
