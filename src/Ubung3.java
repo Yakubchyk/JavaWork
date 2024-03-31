33678371dee883d99b0a6b141a15c3ff15bafa68
Yakubchyk <ivan.yakubchyk@gmail.com>
2024-03-31 18:19:57 +0200

    poslednaja versija

---
+++ src/Ubung3.java
@@ -0,0 +1,26 @@
+public class Ubung3 {
+    public static void main(String[] args) {
+        int i = 100;
+        if (i == 100) {
+            System.out.println("Сто " + i);
+        }
+    }
+}
+
+// крамный . Я не слежу за файлами
+// зеленый . Проиндексированный.
+// git/+add после выделения нужного файла(ов)
+
+//  синий . Git следит. Он изменен.
+class Vania {
+    public static void main(String[] args) {
+        String ivan = "Ivan";
+        System.out.println(ivan);
+    }
+}
+
+class FixMain {
+    public static void main(String[] args) {
+
+    }
+}
